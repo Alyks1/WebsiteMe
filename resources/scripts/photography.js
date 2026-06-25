9d33387f83a177ec91b63852355ce6a806ed02c4
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2026-06-25 23:48:18 +0200

    (feat) improves UI and displays 6 images

--- resources/scripts/photography.js
+++ resources/scripts/photography.js
@@ -4,12 +4,20 @@ fetch('https://api.github.com/repos/Alyks1/WebsiteMe/contents/photos/highlights'
         const images = files
             .filter(f => f.type === 'file' && f.name.match(/\.(jpg|jpeg|png|gif|webp)$/i))
             .sort(() => 0.5 - Math.random())
-            .slice(0, 5);
+            .slice(0, 6);
 
         images.forEach(f => {
             const img = document.createElement('img');
-            img.style.width = '30%';
+            img.onload = () => {
+                img.classList.add('highlight-image');
+                if (img.naturalWidth > img.naturalHeight) {
+                    img.style.marginBottom = '156px';
+                }
+                document.getElementById('highlights').appendChild(img);
+            };
+            
+            
             img.src = f.download_url;
-            document.getElementById('highlights').appendChild(img);
+            
         });
     });
