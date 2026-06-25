f0711170702e75a34848298fc0e51e9d5d926fab
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2026-06-25 20:34:28 +0200

    (fix) adds photography highlights

--- resources/scripts/photography.js
+++ resources/scripts/photography.js
@@ -1,17 +1,15 @@
-fetch('https://api.github.com/repos/Alyks1/WebsiteMe/photos/highlights')
+fetch('https://api.github.com/repos/Alyks1/WebsiteMe/contents/photos/highlights')
     .then(res => res.json())
     .then(files => {
-        let imagesToDisplay = getImagesToDisplay();
-        imagesToDisplay.forEach(f => {
+        const images = files
+            .filter(f => f.type === 'file' && f.name.match(/\.(jpg|jpeg|png|gif|webp)$/i))
+            .sort(() => 0.5 - Math.random())
+            .slice(0, 5);
+
+        images.forEach(f => {
             const img = document.createElement('img');
+            img.style.width = '30%';
             img.src = f.download_url;
-            document.getElementById('gallery').appendChild(img);   
+            document.getElementById('highlights').appendChild(img);
         });
     });
-
-
-function getImagesToDisplay(files) {
-    let images = files.filter(f => f.name.match(/\.(jpg|png|gif|webp)$/i));
-    if (images.length < 5) return images;
-    return images.sort(() => 0.5 - Math.random()).slice(0, 5);
-}
