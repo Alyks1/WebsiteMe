60399b31594541efaa5aadb0d4f8becb73a880a3
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2026-08-09 12:54:39 +0200

    (fix) Adds lazy loading for images and fixes recipe order

--- resources/scripts/photography.js
+++ resources/scripts/photography.js
@@ -1,46 +1,57 @@
 let imageIndex = null;
 
-async function loadImages(folderName) {
+async function getImageIndex() {
     if (!imageIndex) {
         const res = await fetch('index.json');
         imageIndex = await res.json();
     }
+    return imageIndex;
+}
 
-    return imageIndex[folderName].map(name => `photos/${folderName}/${name}`);
+async function loadImages(folderName) {
+    const index = await getImageIndex();
+    return index[folderName].map(name => `photos/${folderName}/${name}`);
+}
+
+function appendImages(container, images, { eagerCount = 0 } = {}) {
+    images.forEach((src, i) => {
+        const img = document.createElement('img');
+        img.classList.add('photos-image');
+        if (i >= eagerCount) {
+            img.loading = 'lazy';
+        }
+        img.src = src;
+        container.appendChild(img);
+    });
 }
 
 async function loadHighlights() {
     const images = await loadImages('highlights');
-
-    images.forEach(image => {
-            const img = document.createElement('img');
-            img.classList.add('photos-image');
-            img.src = image;
-            document.getElementById('highlights').appendChild(img);
-    });
+    appendImages(document.getElementById('highlights'), images, { eagerCount: 5 });
 }
 
-async function loadGallery(fileName) {
-    const images = await loadImages(fileName);
-    const response = await fetch(`subpages/${fileName}.html`);
-    const html = await response.text();
+async function fetchGalleryData(fileName) {
+    const [images, html] = await Promise.all([
+        loadImages(fileName),
+        fetch(`subpages/${fileName}.html`).then(r => r.text())
+    ]);
+    return { fileName, images, html };
+}
+
+function renderGallery({ fileName, images, html }) {
     const div = document.createElement('div');
     div.innerHTML = html;
     document.querySelector('#gallery').appendChild(div);
-
-    images.forEach(image => {
-        const img = document.createElement('img');
-        img.classList.add('photos-image');
-        img.src = image;
-        document.querySelector(`#${fileName}Images`).appendChild(img);
-    });
+    appendImages(document.querySelector(`#${fileName}Images`), images);
 }
 
 loadHighlights();
+
 fetch('recipes.json')
     .then(res => res.json())
-    .then(files => {
-        return Promise.all(files.map(f => loadGallery(f.name)));
+    .then(async files => {
+        const dataList = await Promise.all(files.map(f => fetchGalleryData(f.name)));
+        dataList.forEach(renderGallery);
     })
     .then(() => {
         initLightbox();
